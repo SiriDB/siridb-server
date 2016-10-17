1090992dc55030c2c900c47f038a6a524ead09aa
Jeroen van der Heijden <jvdheijden@insign.it>
2016-10-17 22:20:07 +0200

    Fix time-factor calculation

--- src/siri/db/time.c
+++ src/siri/db/time.c
@@ -76,5 +76,5 @@ uint32_t siridb_time_in_seconds(siridb_t * siridb, int64_t ts)
 uint64_t siridb_time_now(siridb_t * siridb, struct timespec now)
 {
     return now.tv_sec * siridb->time->factor +
-        now.tv_nsec * (siridb->time->factor / 1000000000);
+        now.tv_nsec * (siridb->time->factor / 1000000000.0);
 }
