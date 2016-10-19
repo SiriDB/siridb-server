1239cb6e414b7762cc435de5e1f276449214cd89
Jeroen van der Heijden <jvdheijden@insign.it>
2016-10-19 15:53:53 +0200

    Removed unused project files

--- Debug/src/sdb/args.d
+++
@@ -1,8 +0,0 @@
-src/sdb/args.o: ../src/sdb/args.c ../include/sdb/args.h \
- ../include/argparse/argparse.h ../include/sdb/version.h
-
-../include/sdb/args.h:
-
-../include/argparse/argparse.h:
-
-../include/sdb/version.h:
--- Debug/src/sdb_args/sdb_args.d
+++
@@ -1,6 +0,0 @@
-src/sdb_args/sdb_args.o: ../src/sdb_args/sdb_args.c \
- ../include/argparse/argparse.h ../include/sdb_args/sdb_args.h
-
-../include/argparse/argparse.h:
-
-../include/sdb_args/sdb_args.h:
