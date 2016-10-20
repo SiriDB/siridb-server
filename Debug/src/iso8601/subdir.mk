25f391868c84d2da8fb677d5777311c17e50a8e6
Jeroen van der Heijden <jvdheijden@insign.it>
2016-10-20 11:05:48 +0200

    Use relative path for include

--- Debug/src/iso8601/subdir.mk
+++ Debug/src/iso8601/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/iso8601/%.o: ../src/iso8601/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DDEBUG=1 -I"/home/joente/workspace/siridb-server/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -DDEBUG=1 -I../include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
