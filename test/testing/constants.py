655ace84de2a31569dcf391979e762db3f344b42
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-03-10 23:19:14 +0100

    Update constants.py

--- test/testing/constants.py
+++ test/testing/constants.py
@@ -4,4 +4,4 @@ SIRIDBC = '../{BUILDTYPE}/siridb-server'
 MANAGE = 'siridb-manage'
 # VALGRIND = 'valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes -v '
 VALGRIND = 'valgrind --tool=memcheck '
-MAX_OPEN_FILES = 32768    # Default value is 32768 but with valgrind 512 is max
+MAX_OPEN_FILES = 512    # Default value is 32768 but with valgrind 512 is max
