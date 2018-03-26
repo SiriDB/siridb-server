ee5e2eaa2053f9fbf6518bab765a23279fdf4514
Jeroen van der Heijden <jeroen@transceptor.technology>
2018-03-26 17:10:34 +0200

    Update random script

--- test/siridb-random-data.py
+++ test/siridb-random-data.py
@@ -15,7 +15,7 @@ from siridb.connector import SiriDBClient
 
 
 # Version information
-__version_info__ = (0, 0, 2)
+__version_info__ = (0, 0, 3)
 __version__ = '.'.join(map(str, __version_info__))
 __maintainer__ = 'Jeroen van der Heijden'
 __email__ = 'jeroen@transceptor.technology'
