10f653bcb2905d7504b2016273e19645ef639c9b
Jeroen van der Heijden <jeroen@transceptor.technology>
2018-10-12 13:07:08 +0200

    Rename slist to ver, admin to service and strextra to xstr

---
+++ Release/src/vec/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables
+C_SRCS += \
+../src/vec/vec.c
+
+OBJS += \
+./src/vec/vec.o
+
+C_DEPS += \
+./src/vec/vec.d
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/vec/%.o: ../src/vec/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -DNDEBUG -I../include -O3 -Wall -Wextra $(CPPFLAGS) $(CFLAGS) -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Release/src/xstr/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables
+C_SRCS += \
+../src/xstr/xstr.c
+
+OBJS += \
+./src/xstr/xstr.o
+
+C_DEPS += \
+./src/xstr/xstr.d
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/xstr/%.o: ../src/xstr/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -DNDEBUG -I../include -O3 -Wall -Wextra $(CPPFLAGS) $(CFLAGS) -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
