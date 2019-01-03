c2757bbd1324873f7a1a8f63ab7dfa676bd274a1
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-01-03 14:47:36 +0100

    .

--- grammar/export_grammar.py
+++ grammar/export_grammar.py
@@ -5,7 +5,6 @@ Author: Jeroen van der Heijden (Transceptor Technology)
 Date: 2016-10-10
 '''
 import sys
-sys.path.insert(0, '../../pyleri/')
 import os
 from grammar import siri_grammar
 from pyleri import Grammar
