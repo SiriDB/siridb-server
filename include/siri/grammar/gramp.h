5c020deef2ee09fe0abe00ad533fba9a2411dd4a
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-02-03 13:13:05 +0100

    Update compat with old libcleri

--- include/siri/grammar/gramp.h
+++ include/siri/grammar/gramp.h
@@ -29,8 +29,8 @@
 #define CLERI_NODE_DATA(__node) ((intptr_t)(__node)->data)
 #define CLERI_NODE_DATA_ADDR(__node) ((intptr_t *) &(__node)->data)
 #else
-#define CLERI_NODE_DATA(__node) ((intptr_t)(__node)->result)
-#define CLERI_NODE_DATA_ADDR(__node) ((intptr_t *) &(__node)->result)
+#define CLERI_NODE_DATA(__node) (__node)->result
+#define CLERI_NODE_DATA_ADDR(__node) &(__node)->result
 #endif
 
 #endif  /* SIRI_GRAMP_H_ */
